36021e5fa8352cd79953ab0e39133f46b5f33e0d
Kiran Shinde- WellSky <59969894+KiranShinde-WellSky@users.noreply.github.com>
2021-04-23 17:08:39 +0530

    added mini cards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Covid19dashboardComponent } from './components/covid-19-dashboard/covid19dashboard/covid19dashboard.component';
 
-const routes: Routes = [{ path: 'dashboard', component: Covid19dashboardComponent }];
+const routes: Routes = [
+  { path: '', redirectTo:'/dashboard',pathMatch:'full'},
+  { path: 'dashboard', component: Covid19dashboardComponent }
+];
  
 
 @NgModule({
--- src/app/components/navigation/nav/nav.component.ts
+++ src/app/components/navigation/nav/nav.component.ts
@@ -20,7 +20,7 @@ export class NavComponent implements OnInit{
 
   constructor(private breakpointObserver: BreakpointObserver) {}
   ngOnInit(): void {
-    this.menuItems = ['Supplies & Cases'];
+    this.menuItems = ['dashboard'];
   }
 
   
